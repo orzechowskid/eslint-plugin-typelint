6feec7eeff5e05b642a18940aadf942d152275a1
Dan Orzechowski <orzechod@gmail.com>
2020-05-02 12:25:08 -0400

    typo

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     ],
     "parserOptions": {
         "ecmaFeatures": {
-            "impliedStrinct": true
+            "impliedStrict": true
         },
         "ecmaVersion": 8
     },
